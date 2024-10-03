f4e7cb8a0450fb313e04b39ab97d9fb312ce003b
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-04 01:32:40 +0700

    asdfasdf123123123

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,11 +44,6 @@ class RegisteredUserController extends Controller
 
 
 
-    /**
-     * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
