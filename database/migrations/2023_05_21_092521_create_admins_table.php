8c3ead77d1901f7c5c55ab6d13d0cc3785d3792c
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-05 06:58:49 +0700

    zczxcvzxwererwer

--- database/migrations/2023_05_21_092521_create_admins_table.php
+++ database/migrations/2023_05_21_092521_create_admins_table.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('pj_id')->constrained('admins');
+            $table->text('image');
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('tim', ['DS', 'PKH', 'MM', 'Asyiah', 'Parpol', 'JJ'])->nullable();
+            $table->boolean('status')->default(1);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
