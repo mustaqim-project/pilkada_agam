bc31e50f08cbdec92b3c1ce4dc1cf7a86bf0eead
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-05 13:46:43 +0700

    asdlkfasljhro2342h3nlkfnssljdfnsdf

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
-
 Route::get('/dashboard', function () {
     return view('mobile.frontend.dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
