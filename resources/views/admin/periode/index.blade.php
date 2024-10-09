672b77dac51450df8c36cac6ae88e36541a9e04c
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-09 22:36:34 +0700

    sdfgsdfg4335dfgdfg

--- resources/views/admin/periode/index.blade.php
+++ resources/views/admin/periode/index.blade.php
@@ -157,9 +157,6 @@
         </div>
     </div>
 
-    <!-- Menambahkan jQuery dan Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
     <script>
         $(document).ready(function() {
