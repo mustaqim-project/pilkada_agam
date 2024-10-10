7bf6ed19dd856c779d6eef37ebeb3a65a83fb0df
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-10 23:45:21 +0700

    FGHJFGHJ45645GHJGHJ

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -34,7 +34,6 @@ class ReportController extends Controller
         {
             $admin = Auth::guard('admin')->user();
             $assignees = Admin::where('id', $admin->atasan_id)->get();
-            dd($assignees);
             return view('admin.reports.create', compact('assignees'));
         }
 
