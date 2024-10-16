3bc47416dd3825238c8279950d5aa23984e82d6e
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-16 18:54:50 +0700

    dfghdfghdfghj

--- resources/views/admin/keuangan/penggunaan_anggaran/index.blade.php
+++ resources/views/admin/keuangan/penggunaan_anggaran/index.blade.php
@@ -109,37 +109,33 @@
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    @foreach ($laporanPembayaran as $item)
+                                                    @foreach ($detailPembiayaan as $laporan)
                                                             <tr>
-                                                                <td>{{ $item->detailPembiayaan->nama_rincian }}</td>
-                                                                <td>{{ 'Rp' . number_format($item->jumlah_digunakan, 0, ',', '.') }}</td>
-                                                                <td>{{ $item->status_pembayaran == 1 ? 'Lunas' : 'Belum Lunas' }}</td>
-                                                                <td><img src="{{ asset($item->bukti_pembayaran) }}" alt="Bukti" width="100"></td>
-                                                                <td>{{ $item->keterangan }}</td>
-
+                                                                <td>{{ $laporan->nama_rincian }}</td>
+                                                                <td>Rp {{ number_format($laporan->jumlah_digunakan, 0, ',', '.') }}</td>
                                                                 <td>
-                                                                    <div class="btn-group" role="group" aria-label="Tombol Aksi">
-                                                                        <!-- Tombol Detail -->
-                                                                        <a href="#" data-toggle="modal"
-                                                                            data-target="#detailModal{{ $item->id }}" class="btn btn-primary"
-                                                                            aria-label="Lihat Detail {{ $item->name }}">
-                                                                            <i class="fas fa-eye"></i>
-                                                                        </a>
-                                                                        <!-- Tombol Edit -->
-                                                                        <a href="#" data-toggle="modal"
-                                                                            data-target="#editModal{{ $item->id }}" class="btn btn-warning">
-                                                                            <i class="fas fa-edit"></i>
-                                                                        </a>
-                                                                        <!-- Tombol Hapus -->
-                                                                        <a href="{{ route('admin.keuangan.penggunaan_anggaran.destroy', $item->id) }}"
-                                                                            class="btn btn-danger delete-item">
-                                                                            <i class="fas fa-trash-alt"></i>
-                                                                        </a>
-
-                                                                    </div>
+                                                                    @if ($laporan->status_pembayaran == 1)
+                                                                        Lunas
+                                                                    @elseif($laporan->status_pembayaran == 0)
+                                                                        Belum Dibayar
+                                                                    @else
+                                                                        Status Tidak Diketahui
+                                                                    @endif
+                                                                </td>
+                                                                <td>
+                                                                    @if ($laporan->bukti_pembayaran)
+                                                                        <a href="{{ asset($laporan->bukti_pembayaran) }}" target="_blank">Download</a>
+                                                                    @else
+                                                                        -
+                                                                    @endif
                                                                 </td>
                                                             </tr>
-
+                                                        @else
+                                                            <!-- Jika $laporan tidak valid, Anda bisa menampilkan baris kosong atau pesan -->
+                                                            <tr>
+                                                                <td colspan="4">Tidak ada rincian untuk periode ini.</td>
+                                                            </tr>
+                                                        @endif
                                                     @endforeach
                                                 </tbody>
 
