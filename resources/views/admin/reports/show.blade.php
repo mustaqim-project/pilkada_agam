dc20f32d1649bc3c638430f1f801b8a45d80e510
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-17 20:32:05 +0700

    asdfasdf113241324sdf

--- resources/views/admin/reports/show.blade.php
+++ resources/views/admin/reports/show.blade.php
@@ -5,7 +5,7 @@
     <h1>Detail Laporan</h1>
 
     <h2>Isi Laporan</h2>
-    <div>{!! nl2br(e($report->report_content)) !!}</div>
+    <div>{!! $report->report_content !!}</div>
 
     <h3>Periode: {{ $report->period }}</h3>
     <h4>Dari: {{ $report->creator->name }}</h4>
@@ -13,7 +13,8 @@
 
     @if ($report->attachment)
         <h4>Lampiran:</h4>
-        <div>{!! nl2br(e($report->attachment)) !!}</div>
+        <div>{!! $report->attachment !!}</div>
+
     @endif
 
     <a href="{{ route('admin.reports.index') }}" class="btn btn-secondary">Kembali</a>
