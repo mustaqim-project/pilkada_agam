84788983a1cf08ec7cd127daf4bb139cbcb89c65
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-18 17:35:06 +0700

    akdshkh2b3lk4jhk234

--- resources/views/admin/reports/show.blade.php
+++ resources/views/admin/reports/show.blade.php
@@ -3,39 +3,44 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <a href="{{ route('admin.reports.index') }}" class="btn btn-secondary">Kembali</a>
+            <a href="{{ route('admin.reports.index') }}" class="btn btn-primary">Kembali</a>
         </div>
 
         <div class="card card-primary">
-
             <div class="section-header">
                 <h1>{{ __('admin.Detail Laporan') }}</h1>
             </div>
 
-            <div class="section-header">
-                <h1>{{ __('admin.Periode') }} : {{ $report->period }}</h1>
-            </div>
+            <table class="table">
+                <tr>
+                    <th>{{ __('admin.Periode') }}</th>
+                    <td>{{ $report->period }}</td>
+                </tr>
+                <tr>
+                    <th>{{ __('admin.Dari') }}</th>
+                    <td>{{ $report->creator->name }}</td>
+                </tr>
+                <tr>
+                    <th>{{ __('admin.Dikirim ke') }}</th>
+                    <td>{{ $report->assignee->name }}</td>
+                </tr>
 
-            <div class="section-header">
-                <h1>{{ __('admin.Dari: ') }} {{ $report->creator->name }}</h1>
-            </div>
+                @if ($report->attachment)
+                    <tr>
+                        <th>{{ __('admin.Lampiran') }}</th>
+                        <td>{!! $report->attachment !!}</td>
+                    </tr>
+                @endif
+            </table>
 
-            <div class="section-header">
-                <h1>{{ __('admin.Dikirim ke: ') }} {{ $report->assignee->name }}</h1>
-            </div>
-
-            @if ($report->attachment)
-                <div class="section-header">
-                    <h1>{{ __('admin.Lampiran') }} </h1>
+            <div class="card mt-4">
+                <div class="card-header">
+                    <h3>{{ __('admin.Isi Laporan') }}</h3>
                 </div>
-                <div>{!! $report->attachment !!}</div>
-            @endif
-
-            <div class="section-header">
-                <h1>{{ __('admin.Isi Laporan') }}</h1>
+                <div class="card-body">
+                    {!! $report->report_content !!}
+                </div>
             </div>
-            <div>{!! $report->report_content !!}</div>
-
         </div>
     </section>
 @endsection
