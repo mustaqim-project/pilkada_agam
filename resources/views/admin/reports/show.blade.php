94bc1fe8bd9ebe288a6a74b4f2ba43e7faf46148
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-18 17:29:24 +0700

    ALSDHJFKASJHDJH2J3H4L2H34LJHKLJSDF

--- resources/views/admin/reports/show.blade.php
+++ resources/views/admin/reports/show.blade.php
@@ -1,22 +1,25 @@
 @extends('admin.layouts.master')
 
 @section('content')
-<div class="container">
-    <h1>Detail Laporan</h1>
+    <section class="section">
+        <div class="section-header">
+            <a href="{{ route('admin.reports.index') }}" class="btn btn-secondary">Kembali</a>
+        </div>
 
-    <h2>Isi Laporan</h2>
-    <div>{!! $report->report_content !!}</div>
+        <div class="card card-primary">
+            <h1>Detail Laporan</h1>
+            <h3>Periode: {{ $report->period }}</h3>
+            <h4>Dari: {{ $report->creator->name }}</h4>
+            <h4>Dikirim ke: {{ $report->assignee->name }}</h4>
 
-    <h3>Periode: {{ $report->period }}</h3>
-    <h4>Dari: {{ $report->creator->name }}</h4>
-    <h4>Dikirim ke: {{ $report->assignee->name }}</h4>
+            @if ($report->attachment)
+                <h4>Lampiran:</h4>
+                <div>{!! $report->attachment !!}</div>
+            @endif
 
-    @if ($report->attachment)
-        <h4>Lampiran:</h4>
-        <div>{!! $report->attachment !!}</div>
+            <h2>Isi Laporan</h2>
+            <div>{!! $report->report_content !!}</div>
 
-    @endif
-
-    <a href="{{ route('admin.reports.index') }}" class="btn btn-secondary">Kembali</a>
-</div>
+        </div>
+    </section>
 @endsection
