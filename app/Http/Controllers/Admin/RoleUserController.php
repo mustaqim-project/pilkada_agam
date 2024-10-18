fa39c6dde1208d96537f23d45d66b39b770e7f17
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-18 17:55:33 +0700

    asdjlfnkj23i4b2k34

--- app/Http/Controllers/Admin/RoleUserController.php
+++ app/Http/Controllers/Admin/RoleUserController.php
@@ -95,13 +95,14 @@ class RoleUserController extends Controller
     public function edit(string $id): View
     {
         $roles = Role::all();
-        $admins = Admin::all();
         $teams = tim::all(); // Fetching teams
         $positions = jabatan::all(); // Fetching Jabatan positions
-        $banks = Bank::all(); // Fetching Jabatan positions
-        $user = Admin::findOrFail($id);
-        $roles = Role::all();
-        return view('admin.role-user.create', compact('user', 'roles', 'admins', 'teams', 'positions', 'banks'));
+        $banks = Bank::all(); // Fetching bank data
+        $user = Admin::findOrFail($id); // Fetching user to edit
+
+
+        dd($user);
+        return view('admin.role-user.edit', compact('user', 'roles', 'teams', 'positions', 'banks'));
     }
 
     /**
