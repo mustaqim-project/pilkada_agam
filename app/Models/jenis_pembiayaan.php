351277fefe05919b4d067f00151ca61fe05518fe
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-10-21 10:14:54 +0700

    alsdknfjlk23j42k3j4b

--- app/Models/jenis_pembiayaan.php
+++ app/Models/jenis_pembiayaan.php
@@ -4,18 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\DetailPembiayaan;
 
 class jenis_pembiayaan extends Model
 {
     use HasFactory;
+
     protected $table = 'jenis_pembiayaan';
     protected $primaryKey = 'id';
 
     protected $fillable = [
         'nama_pembiayaan',
     ];
+
     protected $casts = [
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];
+
+    // Definisikan relasi one-to-many dengan DetailPembiayaan
+    public function detailPembiayaan()
+    {
+        return $this->hasMany(DetailPembiayaan::class, 'jenis_pembiayaan_id');
+    }
 }
